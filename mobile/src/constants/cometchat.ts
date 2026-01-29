a015612a9312f49a6c6fabdd87521c25b77cfb9d
tjsinghbubble <tjsingh@trybubble.io>
2026-01-29 19:08:19 +0000

    cometchat

--- mobile/src/constants/cometchat.ts
+++ mobile/src/constants/cometchat.ts
@@ -1,5 +1,5 @@
 export const COMETCHAT_CONSTANTS = {
-  APP_ID: process.env.COMETCHAT_APP_ID || '',
+  APP_ID: '1673948f1ffba3646' || '',
   REGION: process.env.COMETCHAT_REGION || 'us',
-  AUTH_KEY: process.env.COMETCHAT_AUTH_KEY || '',
+  AUTH_KEY: 'bc86ba8ce7fa18c4bdb47dfc25b9a0474195e189' || '',
 };
